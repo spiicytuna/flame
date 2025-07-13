d3edaa945ecbe7d44b2fec3964e3e0c97e76f8b6
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-07-13 22:03:58 +0000

    fix: the path was wrong when moving from bind-mounted files to github in docker-compose

--- utils/weather.js
+++ utils/weather.js
@@ -1,17 +1,25 @@
 const fetch = require('node-fetch');
 // const API_KEY = process.env.WEATHER_API_KEY || ''; // swap with below if you want to pull the weather API key from the docker-compose
-const loadConfig = require('../utils/loadConfig');
-const config = loadConfig();
-
-const API_KEY = config.WEATHER_API_KEY || process.env.WEATHER_API_KEY || '';
-if (!API_KEY) throw new Error('Missing WEATHER_API_KEY in config.json > add in Dashboard settings');
+// const loadConfig = require('../utils/loadConfig');
+const loadConfig = require('./loadConfig');
 
 const API_URL = 'https://api.weatherapi.com/v1/current.json';
 
-async function getWeather(lat, lon) {
-  if (!API_KEY) throw new Error('Weather API key missing');
+let API_KEY = ''; // lazy-loaded
 
-  const url = `${API_URL}?key=${API_KEY}&q=${lat},${lon}&aqi=no`;
+function getAPIKey() {
+  if (!API_KEY) {
+    const config = loadConfig();
+    API_KEY = config.WEATHER_API_KEY || process.env.WEATHER_API_KEY || '';
+    if (!API_KEY) throw new Error('Missing WEATHER_API_KEY in config.json > add in Dashboard settings');
+  }
+  return API_KEY;
+}
+
+async function getWeather(lat, lon) {
+  const key = getAPIKey();
+
+  const url = `${API_URL}?key=${key}&q=${lat},${lon}&aqi=no`;
   const res = await fetch(url);
   const data = await res.json();
 
