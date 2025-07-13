47257aecff6d5946414376d702cf9351f32601d0
spiicytuna <git@spiicytuna.org>
2025-07-13 22:36:28 +0000

    fix: await vs. asynchronous

--- utils/weather.js
+++ utils/weather.js
@@ -1,23 +1,24 @@
 const fetch = require('node-fetch');
 // const API_KEY = process.env.WEATHER_API_KEY || ''; // swap with below if you want to pull the weather API key from the docker-compose
-// const loadConfig = require('../utils/loadConfig');
 const loadConfig = require('./loadConfig');
 
 const API_URL = 'https://api.weatherapi.com/v1/current.json';
 
-let API_KEY = ''; // lazy-loaded
+let API_KEY = ''; // cache the key once loaded
 
-function getAPIKey() {
+async function getAPIKey() {
   if (!API_KEY) {
-    const config = loadConfig();
+    const config = await loadConfig();
     API_KEY = config.WEATHER_API_KEY || process.env.WEATHER_API_KEY || '';
-    if (!API_KEY) throw new Error('Missing WEATHER_API_KEY in config.json > add in Dashboard settings');
+    if (!API_KEY) {
+      throw new Error('Missing WEATHER_API_KEY in config.json > add in Dashboard settings');
+    }
   }
   return API_KEY;
 }
 
 async function getWeather(lat, lon) {
-  const key = getAPIKey();
+  const key = await getAPIKey();
 
   const url = `${API_URL}?key=${key}&q=${lat},${lon}&aqi=no`;
   const res = await fetch(url);
