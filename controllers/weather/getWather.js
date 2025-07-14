172b9668d30a150bca7ac8e4abef950bfc8ec6c2
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-07-14 15:51:45 +0000

    update: backend changes to lookup weather based on GUI settings of GeoIP location  or Fixed lat/lon

--- controllers/weather/getWather.js
+++ controllers/weather/getWather.js
@@ -1,13 +1,23 @@
 const { getGeoFromIP } = require('../../utils/geoIP');
 const { getWeather } = require('../../utils/weather');
+const loadConfig = require('../../utils/loadConfig');
 
 module.exports = async (req, res) => {
   try {
-    const ip = (req.headers['x-forwarded-for'] || req.socket.remoteAddress || '').split(',')[0].trim();
-    const { lat, lon } = await getGeoFromIP(ip);
+    const config = await loadConfig();
+    let lat, lon;
 
-    const weather = await getWeather(lat, lon); // ✅ Fixed function name
+    if (config.weatherMode === 'fixed') {
+      lat = config.lat;
+      lon = config.long;
+    } else {
+      const ip = (req.headers['x-forwarded-for'] || req.socket.remoteAddress || '').split(',')[0].trim();
+      const loc = await getGeoFromIP(ip);
+      lat = loc?.lat || config.lat;
+      lon = loc?.lon || config.long;
+    }
 
+    const weather = await getWeather(lat, lon);
     res.status(200).json({ success: true, data: [weather] });
   } catch (err) {
     console.error('[Weather API]', err);
