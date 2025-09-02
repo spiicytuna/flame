715d00ac79ed38679c8e53a0e4528138e3eec005
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-09-02 06:38:21 +0000

    Centralize and decouple category state management from bookmarks
    Previously, all state and actions for categories were solely coupled to the bookmarks feature. When categories were
    added to Applications as well originally all of the bookmark actons/state were reused for proof of concept.  After
    a it was kind of working it was realized this made it difficult to reuse the category logic for Applications and
    beyond that created a confusion.
    
    This refactor accomplishes the following:
    - Creates a new, dedicated 'categories' Redux slice to act as the single source of truth for all category data.
    - Moves all category-related action creators and reducer logic from the 'bookmarks' slice to the new 'categories' slice.
    - Updates all components (Home, Apps, Bookmarks, and their sub-components) to use the new centralized state and actions.

--- client/src/store/actions/categoryActions.ts
+++ client/src/store/actions/categoryActions.ts
@@ -1,47 +1,73 @@
-// client/src/store/actions/categoryActions.ts
 import { ActionType } from '../action-types';
-import { Category } from '../../interfaces';
+import { App, Category, Bookmark } from '../../interfaces';
 
+export interface GetCategories {
+  type: ActionType.getCategories;
+}
 export interface GetCategoriesSuccess {
   type: ActionType.getCategoriesSuccess;
-  payload: Category[];
+  payload: {
+    categories: Category[];
+    section: 'apps' | 'bookmarks';
+  };
 }
-
+export interface GetCategoriesError {
+  type: ActionType.getCategoriesError;
+  payload: string;
+}
 export interface AddCategory {
   type: ActionType.addCategory;
   payload: Category;
 }
-
 export interface UpdateCategory {
   type: ActionType.updateCategory;
   payload: Category;
 }
-
 export interface DeleteCategory {
   type: ActionType.deleteCategory;
   payload: number;
 }
-
 export interface SetEditCategory {
   type: ActionType.setEditCategory;
   payload: Category | null;
 }
-
 export interface PinCategory {
   type: ActionType.pinCategory;
   payload: Category;
 }
-
 export interface ReorderCategories {
   type: ActionType.reorderCategories;
   payload: Category[];
 }
+export interface AddBookmark {
+  type: ActionType.addBookmark;
+  payload: Bookmark;
+}
+export interface DeleteBookmark {
+  type: ActionType.deleteBookmark;
+  payload: {
+    bookmarkId: number;
+    categoryId: number;
+  };
+}
+export interface FetchHomepageDataSuccess {
+  type: ActionType.fetchHomepageDataSuccess;
+  payload: {
+    apps: App[];
+    categories: Category[];
+  };
+}
 
 export type CategoryAction =
+  | FetchHomepageDataSuccess
+  | GetCategories
   | GetCategoriesSuccess
+  | GetCategoriesError
   | AddCategory
   | UpdateCategory
   | DeleteCategory
   | SetEditCategory
   | PinCategory
-  | ReorderCategories;
+  | ReorderCategories
+  | AddBookmark
+  | DeleteBookmark;
