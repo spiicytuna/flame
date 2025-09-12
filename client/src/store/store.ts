d3cfa3c7f1c9189c27f87e64e4815d009797af47
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-09-12 05:01:42 +0000

    FIX: Complete security overhaul and modernization
    Addressed dozens of critical and high-severity vulnerabilities
    
    - Upgraded Node.js version (16) => LTS release (22)
    - Migrated frontend Create React App to Vite => improves build performance and security
    - Updated all server-side and client-side npm deps to latest => resolved all `npm audit`
    - Upgraded major libraries => React (17 to 18) + React Router (v5 to v6)
    - Refactors codebase => support new library versions + React 18 root API => modernizing routing

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import { reducers } from './reducers';
+
 
 export const store = createStore(
   reducers,
