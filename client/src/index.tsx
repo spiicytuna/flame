bfe66dd017551de8902385d969aed0c4ef3fecbe
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-09-12 05:08:52 +0000

    FIX: Complete security overhaul and modernization
    Addressed dozens of critical and high-severity vulnerabilities
    
    - Upgraded Node.js version (16) => LTS release (22)
    - Migrated frontend Create React App to Vite => improves build performance and security
    - Updated all server-side and client-side npm deps to latest => resolved all `npm audit`
    - Upgraded major libraries => React (17 to 18) + React Router (v5 to v6)
    - Refactors codebase => support new library versions + React 18 root API => modernizing routing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
+// CSS
+import './index.css';
+
+// Components
 import { App } from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!); // new for React 18 API
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
